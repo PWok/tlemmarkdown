a0d0a6c9b135f62cf6fef56d01d1e956339d13a1
Wojciech Orański <wojtekor9@gmail.com>
2024-02-10 22:41:32 +0100

    a very primitive (WORKING!) version

--- tlemmarkdown/__init__.py
+++ tlemmarkdown/__init__.py
@@ -1,8 +1,22 @@
+import sys
 import markdown
+from .mdpreprocessor import CodeBlockExtension
 
-from pygments import highlight
-from pygments.lexers import Python3Lexer
-from pygments.formatters import HtmlFormatter
 
-code = 'print "Hello World"'
-print(highlight(code, Python3Lexer(), HtmlFormatter()))
+def main(source, output, * ,source_encoding, output_encoding):
+    try:
+        if source is None:
+            html = input()
+        else:
+            with open(source, "r", encoding=source_encoding) as f:
+                # html = markdown.markdown(f.read())
+                html = markdown.markdown(f.read(), extensions=[CodeBlockExtension()])
+
+        if output is None:
+            print(html)
+        else:
+            with open(output, "w", encoding=output_encoding) as f:
+                f.write(html)
+        return 0
+    except Exception as e:
+        sys.exit(e)
