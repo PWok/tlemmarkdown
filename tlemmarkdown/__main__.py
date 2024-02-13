fb8b7c716d49a9f26b2ce78efc4c7f41efadfb6c
Wojciech Orański <wojtekor9@gmail.com>
2024-02-13 12:02:34 +0100

    fix __main__ function

--- tlemmarkdown/__main__.py
+++ tlemmarkdown/__main__.py
@@ -3,58 +3,62 @@ import argparse
 
 from tlemmarkdown import main
 
-parser = argparse.ArgumentParser(
-    description="Read a markdown file and process it to an html file as used by T-LEM"
-)
-parser.add_argument(
-    "-s",
-    "--source",
-    dest="source",
-    type=str,
-    default=None,
-    help="The markdown file. Stdin by default",
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    dest="output",
-    type=str,
-    default=None,
-    help="The resulting html file. Stdout by default.",
-)
-parser.add_argument(
-    "-c",
-    "--code-style",
-    dest="code_style",
-    type=str,
-    default="monokai",
-    help="""Optional code style name. Can be any style supported by Pygments
-            Defaults to monokai. It should be noted that the background is overwritten.
-            By default the `C++ code` black (hex #141414) background, causing some styles to be unreadble""",
-)
-parser.add_argument(
-    "--source-encoding",
-    dest="source_encoding",
-    type=str,
-    default="UTF-8",
-    help="The text encoding of the source file.",
-)
-parser.add_argument(
-    "--output-encoding",
-    dest="output_encoding",
-    type=str,
-    default="UTF-8",
-    help="The text encoding of the output file.",
-)
-
-
-args = parser.parse_args()
-sys.exit(
-    main(
-        args.source,
-        args.output,
-        source_encoding=args.source_encoding,
-        output_encoding=args.output_encoding,
-        code_style=args.code_style,
+def run():
+    parser = argparse.ArgumentParser(
+        description="Read a markdown file and process it to an html file as used by T-LEM"
     )
-)
+    parser.add_argument(
+        "-s",
+        "--source",
+        dest="source",
+        type=str,
+        default=None,
+        help="The markdown file. Stdin by default",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        type=str,
+        default=None,
+        help="The resulting html file. Stdout by default.",
+    )
+    parser.add_argument(
+        "-c",
+        "--code-style",
+        dest="code_style",
+        type=str,
+        default="monokai",
+        help="""Optional code style name. Can be any style supported by Pygments
+                Defaults to monokai. It should be noted that the background is overwritten.
+                By default the `C++ code` black (hex #141414) background, causing some styles to be unreadble""",
+    )
+    parser.add_argument(
+        "--source-encoding",
+        dest="source_encoding",
+        type=str,
+        default="UTF-8",
+        help="The text encoding of the source file.",
+    )
+    parser.add_argument(
+        "--output-encoding",
+        dest="output_encoding",
+        type=str,
+        default="UTF-8",
+        help="The text encoding of the output file.",
+    )
+
+
+    args = parser.parse_args()
+    sys.exit(
+        main(
+            args.source,
+            args.output,
+            source_encoding=args.source_encoding,
+            output_encoding=args.output_encoding,
+            code_style=args.code_style,
+        )
+    )
+
+if __name__ == "__main__":
+    run()
